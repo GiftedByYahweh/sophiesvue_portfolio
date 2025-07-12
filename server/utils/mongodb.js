528c2f6a5cd8ea70dad6055fff9d4de31935441d
GiftedByYahweh <giftedbyyahweh@gmail.com>
2025-07-12 13:28:04 +0300

    remove mongo list

--- server/utils/mongodb.js
+++
@@ -1,6 +0,0 @@
-export const CATEGORIES_NAME = "categories";
-export const COLLECTIONS_NAME = "collections";
-export const ALBUM_NAME = "photos";
-export const AUTH_NAME = "user";
-export const PROFILE_NAME = "profile";
-export const PRICE_NAME = "price";
