1bd13d0bd1dfbbc7a82e1c225b2032697abb70d5
GiftedByYahweh <giftedbyyahweh@gmail.com>
2026-05-24 01:33:01 +0300

    ercel: route /api/* to bundled api/server.mjs via rewrites

--- admin/api/ping.mjs
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.statusCode = 200;
-  res.setHeader("content-type", "application/json");
-  res.end(JSON.stringify({ url: req.url, headers: req.headers }, null, 2));
-}
